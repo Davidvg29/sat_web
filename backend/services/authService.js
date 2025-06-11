8ce1dadc3d3a20cd4c47a466f38a20729056cb2d
Davidvg29 <davidpivito@gmail.com>
2025-06-11 11:46:03 -0300

    valido password vacio antes de iniciar proceso de comparar

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt")
+const crypto = require('crypto');
 
 const hashPassword = async (password)=>{
     const saltRounds = 10;
@@ -7,13 +8,18 @@ const hashPassword = async (password)=>{
 
 const comparePassword = async (plainPassword, hashedPassword) => {
     // como los usuarios son migrados de php se reemplaza los primeros caracteres del hash
+    if (!plainPassword || !hashedPassword) return false;
+    
     let fixedHash = hashedPassword; 
+
     if(hashedPassword.includes("$2y$")){
         fixedHash = hashedPassword.replace(/^\$2y\$/, "$2b$")
+        return await bcrypt.compare(plainPassword, fixedHash);
     }
-    return await bcrypt.compare(plainPassword, fixedHash);
-};
 
+    const hashMD5 = crypto.createHash('md5').update(plainPassword).digest('hex');
+    return hashMD5 === hashedPassword;
+};
 
 module.exports = {
     hashPassword,
