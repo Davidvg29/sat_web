c30417038d67a2a6ef6d32ab483a66edb7e7cbcb
Davidvg29 <davidpivito@gmail.com>
2025-06-13 10:00:31 -0300

    ordenando informacion de inmueble

--- backend/controllers/inmueble/getInmueble.js
+++ backend/controllers/inmueble/getInmueble.js
@@ -39,7 +39,27 @@ const getInmueble = async (req, res) => {
         return res.status(200).json({
             status: true,
             message: "Inmueble encontrado",
-            informacion: archivoTxt,
+            informacion: {
+                codInmueble: archivoTxt.toString().slice(0, 8),
+                cuenta  : {
+                    distrito: archivoTxt.toString().slice(8, 11),
+                    cuenta: archivoTxt.toString().slice(11, 19),
+                    subCuenta: archivoTxt.toString().slice(19, 23),
+                },
+                nombre: archivoTxt.toString().slice(23, 50).trim(),
+                direccion: {
+                    calle: archivoTxt.toString().slice(73, 103).trim(),
+                    numero: archivoTxt.toString().slice(103, 108).trim(),
+                    piso: archivoTxt.toString().slice(108, 110).trim(),
+                    depto: archivoTxt.toString().slice(110, 113).trim(),
+                    manzana: archivoTxt.toString().slice(113, 116).trim(),
+                    block: archivoTxt.toString().slice(116, 119).trim(),
+                    lote: archivoTxt.toString().slice(119, 122).trim(),
+                    casa: archivoTxt.toString().slice(122, 125).trim(),
+                    barrio: archivoTxt.toString().slice(125, 155).trim(),
+                    localidad: archivoTxt.toString().slice(155, 185).trim(),
+                },
+            },
         });
 
     } catch (error) {
