43ce84493435470b42bb12a0871368b628f66a1e
Davidvg29 <davidpivito@gmail.com>
2025-06-18 12:16:38 -0300

    configuramos cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const cors = require("cors")
 
 app.use(express.json());
+
+app.use(cors())
 
 // rutas
 app.use('/api', routes);
