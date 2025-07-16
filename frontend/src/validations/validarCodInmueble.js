4f0db0a094b1d8859e5aec85ea15ce178d56903b
Davidvg29 <davidpivito@gmail.com>
2025-07-16 11:57:53 -0300

    se agrega funcion para validar codigo de inmueble

---
+++ frontend/src/validations/validarCodInmueble.js
@@ -0,0 +1,14 @@
+const validarCodInmueble = (codInmueble)=>{
+    if(codInmueble.length !== 8 || isNaN(codInmueble)){
+        return false
+    }
+    
+    const agencias = [164, 165, 166, 167, 168, 169, 182, 183, 184, 186, 187, 189, 190, 191, 192, 193]
+    const verificaCodAgencia = agencias.includes(parseInt(codInmueble.slice(0, 3)))
+    if(!verificaCodAgencia){
+        return false
+    }
+
+    return true
+}
+export default validarCodInmueble
