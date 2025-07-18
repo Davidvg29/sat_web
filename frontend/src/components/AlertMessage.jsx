9f8b7f12fd5df0aa57a65bb95cc8cfdb718b43b4
Davidvg29 <davidpivito@gmail.com>
2025-07-18 13:03:46 -0300

    componente AlertMessage muestra mensaje en alerta

--- frontend/src/components/AlertMessage.jsx
+++ frontend/src/components/AlertMessage.jsx
@@ -4,35 +4,51 @@ import {
   AlertDescription,
   AlertTitle,
 } from "@/components/ui/alert"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { alertMessage as setAlertMessage } from "@/redux/action";
 
 const AlertMessage = () => {
+
+    const alertData = useSelector((state)=>state.alertMessage)
+    const [visible, setVisible] = useState(false);
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        console.log("useEffect")
+        if (alertData.active) {
+            setVisible(true);
+            const timer = setTimeout(() => {
+                setVisible(false);
+                dispatch(setAlertMessage(false, null, ""))
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [alertData]);
+
+    if (!visible || !alertData.active) return null
     return ( 
-        <div className="grid w-full max-w-xl items-start gap-4">
-            <Alert>
-                <CheckCircle2Icon />
-                <AlertTitle>Success! Your changes have been saved</AlertTitle>
-                {/* <AlertDescription>
-                This is an alert with icon, title and description.
-                </AlertDescription> */}
-            </Alert>
-            {/* <Alert>
-                <PopcornIcon />
-                <AlertTitle>
-                This Alert has a title and an icon. No description.
-                </AlertTitle>
-            </Alert> */}
-            {/* <Alert variant="destructive">
-                <AlertCircleIcon />
-                <AlertTitle>Unable to process your payment.</AlertTitle>
-                <AlertDescription>
-                <p>Please verify your billing information and try again.</p>
-                <ul className="list-inside list-disc text-sm">
-                    <li>Check your card details</li>
-                    <li>Ensure sufficient funds</li>
-                    <li>Verify billing address</li>
-                </ul>
-                </AlertDescription>
-            </Alert> */}
+        <div className="grid w-full max-w-xl items-start fixed bottom-10 inset-x-0 mx-auto pl-5 pr-5 z-1000">
+            {alertData.status ? (
+                <Alert className="bg-green-100">
+                    <CheckCircle2Icon />
+                    <AlertTitle className="text-green-800">{alertData.message}</AlertTitle>
+                    {/* <AlertDescription>
+                    This is an alert with icon, title and description.
+                    </AlertDescription> */}
+                </Alert>
+            ):(
+                <Alert variant="destructive" className="bg-red-100">
+                    <AlertCircleIcon />
+                    <AlertTitle>{alertData.message}</AlertTitle>
+                    <AlertDescription>
+                    {/* <p>Por favor intente mas tarde.</p> */}
+                    </AlertDescription>
+                </Alert>
+            )}
+            
+            
         </div>
      );
 }
